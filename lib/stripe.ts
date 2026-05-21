76b85f58066f6caf385db789fca54e2dae3a6ba1
arinsshha <arinsha666@gmail.com>
2026-05-21 16:11:41 +0530

    Fix Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from "stripe";
 
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2024-10-28.acacia"
+      apiVersion: "2025-02-24.acacia"
     })
   : null;
